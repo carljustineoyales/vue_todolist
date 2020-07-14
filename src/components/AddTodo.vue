eadb05bc8690b3e69738a5ab717c3b0e1f6878d9
Carl Oyales <carljustineoyales@gmail.com>
2020-07-15 00:04:50 +0800

    Initial Commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" v-model="title" name="title" placeholder="Add To Do" />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AddTodo",
+    data() {
+      return {
+        title: ""
+      };
+    },
+    methods: {
+      addTodo(e) {
+        e.preventDefault();
+        const newTodo = {
+          title: this.title,
+          completed: false
+        };
+
+        //Send Up to parent
+        this.$emit("add-todo", newTodo);
+
+        // clear form
+        this.title = "";
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"] {
+    flex: 2;
+  }
+</style>
